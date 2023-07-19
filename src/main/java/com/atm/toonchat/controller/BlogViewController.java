dce3319d653639ce42ea395aa2da5c83c153e831
jaewonkim <resource777@naver.com>
2023-07-19 16:49:31 +0900

    Feat: 글 보기 controller 추가

--- src/main/java/com/atm/toonchat/controller/BlogViewController.java
+++ src/main/java/com/atm/toonchat/controller/BlogViewController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.atm.toonchat.domain.Article;
 import com.atm.toonchat.dto.ArticleListViewResponse;
+import com.atm.toonchat.dto.ArticleViewResponse;
 import com.atm.toonchat.service.BlogService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,5 +28,12 @@ public class BlogViewController {
 		model.addAttribute("articles",articles);
 
 		return "articleList";
+	}
+	@GetMapping("/articles/{id}")
+	public String getArticle(@PathVariable Long id, Model model){
+		Article article = blogService.findById(id);
+		model.addAttribute("article",new ArticleViewResponse(article));
+
+		return "article";
 	}
 }
