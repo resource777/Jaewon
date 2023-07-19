b3c0031ffda8532faa6f6233b15306f72e430fa7
jaewonkim <resource777@naver.com>
2023-07-19 16:22:26 +0900

    Feat: 생성시간 및 수정시간 컬럼 추가

--- src/main/java/com/atm/toonchat/domain/Article.java
+++ src/main/java/com/atm/toonchat/domain/Article.java
@@ -1,5 +1,10 @@
 package com.atm.toonchat.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +27,14 @@ public class Article {
 
 	@Column(name = "content", nullable = false)
 	private String content;
+
+	@CreatedDate
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
 
 	@Builder // 빌더 패턴으로 객체 생성
 	public Article(String title, String content){
