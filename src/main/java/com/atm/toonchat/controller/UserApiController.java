578cfc7e78ad6049eb5f918e0e9a9ddd98fc88e1
jaewonkim <resource777@naver.com>
2023-07-22 17:42:13 +0900

    Feat: 로그아웃 api controller 추가

--- src/main/java/com/atm/toonchat/controller/UserApiController.java
+++ src/main/java/com/atm/toonchat/controller/UserApiController.java
@@ -1,12 +1,17 @@
 package com.atm.toonchat.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.atm.toonchat.dto.AddUserRequest;
 import com.atm.toonchat.service.UserDetailService;
 import com.atm.toonchat.service.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -18,6 +23,13 @@ public class UserApiController {
 	@PostMapping("/user")
 	public String signup(AddUserRequest request){
 		userService.save(request);
+		return "redirect:/login";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response){
+		new SecurityContextLogoutHandler().logout(request,response,
+			SecurityContextHolder.getContext().getAuthentication());
 		return "redirect:/login";
 	}
 }
