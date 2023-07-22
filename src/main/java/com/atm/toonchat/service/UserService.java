88272da1d0a14faf94b47fe1649fdde925fc077b
jaewonkim <resource777@naver.com>
2023-07-22 17:23:27 +0900

    Feat: UserService 추가

--- src/main/java/com/atm/toonchat/service/UserService.java
+++ src/main/java/com/atm/toonchat/service/UserService.java
@@ -1,10 +1,24 @@
 package com.atm.toonchat.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.atm.toonchat.domain.User;
+import com.atm.toonchat.dto.AddUserRequest;
+import com.atm.toonchat.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public Long save(AddUserRequest dto){
+		return userRepository.save(User.builder()
+			.email(dto.getEmail())
+			.password(bCryptPasswordEncoder.encode(dto.getPassword()))
+			.build()).getId();
+	}
 }
